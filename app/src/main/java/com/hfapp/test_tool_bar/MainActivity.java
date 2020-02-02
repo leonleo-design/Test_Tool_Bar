269951e364bfad295cef00a73ebfd7045bb24886
leonleo-design <59860807+leonleo-design@users.noreply.github.com>
2020-02-02 15:11:47 +0800

    initial commit

---
+++ app/src/main/java/com/hfapp/test_tool_bar/MainActivity.java
@@ -0,0 +1,108 @@
+package com.hfapp.test_tool_bar;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import android.content.Context;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        SectionsPagerAdapter pagerAdapter=new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager=findViewById(R.id.view_pager);
+        pager.setAdapter(pagerAdapter);
+        TabLayout tabLayout=findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter{
+        public SectionsPagerAdapter(FragmentManager fragmentManager){
+            super(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+        @Override
+        public int getCount(){
+            return 4;
+        }
+        @Override
+        public Fragment getItem(int positon){
+            switch(positon){
+                case 0:
+                    return new TopFragment();
+                case 1:
+                    return new PizzaFragment();
+                case 2:
+                    return new PastaFragment();
+                case 3:
+                    return new StoresFragment();
+            }
+            return null;
+        }
+        @Override
+        public CharSequence getPageTitle(int position){
+            switch (position){
+                case 0:
+                    return getResources().getText(R.string.home_tab);
+                case 1:
+                    return getResources().getText(R.string.pizza_tab);
+                case 2:
+                    return getResources().getText(R.string.pasta_tab);
+                case 3:
+                    return getResources().getText(R.string.store_tab);
+
+            }
+            return null;
+        }
+
+
+   }
+}
+
+
---
+++ app/src/main/java/com/hfapp/test_tool_bar/StoresFragment.java
@@ -0,0 +1,30 @@
+package com.hfapp.test_tool_bar;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StoresFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(
+                inflater.getContext(),android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater,container,savedInstanceState);
+
+
+    }
+
+}
